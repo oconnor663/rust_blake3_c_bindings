a4f6a76691d17a7ef342dd13304bf03be2091ec1
Jack O'Connor <oconnor663@gmail.com>
2021-07-26 21:08:17 -0400

    turn on optimizations

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e -u -o pipefail -v
 
 cd "$(dirname "$BASH_SOURCE")"
 
-cargo build --target x86_64-unknown-linux-musl
+cargo build --target x86_64-unknown-linux-musl --release
 
 cbindgen --lang c > blake3_bindings.h
 
-musl-gcc -g -Wall -pedantic -o test test.c target/x86_64-unknown-linux-musl/debug/libscratch.a
+musl-gcc -O3 -Wall -pedantic -o test test.c target/x86_64-unknown-linux-musl/release/libscratch.a
